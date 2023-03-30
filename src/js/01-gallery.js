7f139c8cdf4a6b327b1164ab5a9e6a4cfde7825e
barkode <konstantinks@gmail.com>
2023-03-30 11:16:31 +0100

    first end

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,19 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItemsMarkup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img loading="lazy" class="gallery__image" src="${preview}" alt="${description}"/></a></li>`
+  )
+  .join('');
+gallery.style.listStyle = 'none';
+gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
