c209020e049db393e77c430bff3ae5fd0cba0a56
barkode <konstantinks@gmail.com>
2023-04-03 19:22:01 +0100

    second end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+if (getCurrentTime) {
+  player.setCurrentTime(getCurrentTime);
+}
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime(data) {
+  let currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
