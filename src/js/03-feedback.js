85e2ad171c66114db6c172d872b32cc87499b0c8
barkode <konstantinks@gmail.com>
2023-04-03 20:57:35 +0100

    someting functions add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,32 @@
 import throttle from 'lodash.throttle';
 
-const
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('click', onFormSubmit);
+textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTexteria();
+
+function inputFunction(event) {
+  console.dir(event);
+}
+
+function onTextareaInput(event) {
+  const message = event.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTexteria() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    textarea.value = savedMessage;
+  }
+}
