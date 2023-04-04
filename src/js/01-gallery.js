0f4ff8680f1619e824f4d32fcb14a80f29be7816
barkode <konstantinks@gmail.com>
2023-04-04 20:06:59 +0100

    fix gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,14 +4,12 @@ import simpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector('.gallery');
 
 const galleryItemsMarkup = galleryItems
   .map(
     ({ original, preview, description }) =>
-      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img loading="lazy" class="gallery__image" src="${preview}" alt="${description}"/></a></li>`
+      `<a class="gallery__link" href="${original}"><img loading="lazy" class="gallery__image" src="${preview}" alt="${description}"/></a>`
   )
   .join('');
 gallery.style.listStyle = 'none';
