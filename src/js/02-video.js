4bd6ce3456f289f06187fe10cc9253669e333110
barkode <konstantinks@gmail.com>
2023-04-04 16:17:03 +0100

    fix to 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+const DELAY_KEY = 1000;
 
 const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
-const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+const getCurrentTime = localStorage.getItem(CURRENT_TIME_KEY);
 
 if (getCurrentTime) {
   player.setCurrentTime(getCurrentTime);
 }
 
-player.on('timeupdate', throttle(currentTime, 1000));
+player.on('timeupdate', throttle(currentTime, DELAY_KEY));
 
 function currentTime(data) {
   let currentTime = Math.round(data.seconds);
-  localStorage.setItem('videoplayer-current-time', currentTime);
+  localStorage.setItem(CURRENT_TIME_KEY, currentTime);
 }
